2a46f0281b03664729239c56de21d5698b2aeae6
Muhammad Habibullah <muhammadhabibullah.id@gmail.com>
2023-10-21 10:47:19 +0700

    axis

---
+++ chess/axis/axis.go
@@ -0,0 +1,29 @@
+package axis
+
+type Axis int
+type X Axis
+type Y Axis
+
+const (
+	_ X = iota
+	XA
+	XB
+	XC
+	XD
+	XE
+	XF
+	XG
+	XH
+)
+
+const (
+	_ Y = iota
+	Y1
+	Y2
+	Y3
+	Y4
+	Y5
+	Y6
+	Y7
+	Y8
+)
